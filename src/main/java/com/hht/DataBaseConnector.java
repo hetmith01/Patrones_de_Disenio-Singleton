e5dc65ecb81338e356833dc5e9d254c5447b0e7f
Hetmith <het.hanc.14@gmail.com>
2025-02-07 00:05:02 -0500

    Patron singleton terminado

--- src/main/java/com/hht/DataBaseConnector.java
+++ src/main/java/com/hht/DataBaseConnector.java
@@ -1,4 +1,33 @@
 package com.hht;
 
+
+/*
+ * 1) Tener un constructor privado
+ * 2) Tener un otributo privado  además estatico
+ * 3) Tener un método publico estatico que devuelva la clase
+ */
+
+
 public class DataBaseConnector {
+
+    private static DataBaseConnector dataBaseConnector;
+
+    private DataBaseConnector(){
+        System.out.println("Creación de objeto");
+    }
+
+    public static DataBaseConnector getInstance(){
+      if(dataBaseConnector == null){
+          dataBaseConnector = new DataBaseConnector();
+      }
+      return dataBaseConnector;
+    };
+
+    public void connectDAtaBase(){
+        System.out.println("conectando al a base de datos "+ dataBaseConnector);
+    }
+
+    public void disconnectDAtaBase(){
+        System.out.println("desconectando al a base de datos "+ dataBaseConnector);
+    }
 }
--- src/main/java/com/hht/Main.java
+++ src/main/java/com/hht/Main.java
@@ -1,7 +1,17 @@
 package com.hht;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        DataBaseConnector dataBaseConnector = DataBaseConnector.getInstance();
+        System.out.println("el objeto databaseConnector es : "+ dataBaseConnector);
+
+        DataBaseConnector dataBaseConnector1 = DataBaseConnector.getInstance();
+        System.out.println("EL objeto1 es : "+ dataBaseConnector1);
+
+
+        dataBaseConnector.connectDAtaBase();
+
+        dataBaseConnector1.connectDAtaBase();
     }
 }
